8672f78e19f6c6987336afc1f2d9131814fe5e78
HectorColasValtueña <MephistoII666@gmail.com>
2023-11-03 03:49:34 +0100

    weapon scripts moved to weapons folder

--- Assets/Package/PHATASS-UnityUtils/Attributes/SerializedTypeRestrictionAttribute/SampleScene/Scripts/Weapons/IWeapon.cs
+++ Assets/Package/PHATASS-UnityUtils/Attributes/SerializedTypeRestrictionAttribute/SampleScene/Scripts/Weapons/IWeapon.cs
@@ -1,9 +1,9 @@
 namespace PHATASS.Docs.Examples
 {
-	//
-	//	Interface representing a weapon.
-	//	Any class implementing IWeapon can be used as a weapon, by providing an implementation 
-	//
+//
+//	Interface representing a weapon.
+//	Any class implementing IWeapon can be used as a weapon, by providing an implementation 
+//
 	public interface IWeapon
 	{
 		//IWeapon.Shoot() should be given an implementation that makes that weapon shoot
