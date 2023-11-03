8cb8df5f28199e4bc75e7ada133a540d99c8abbc
HectorColasValtueña <MephistoII666@gmail.com>
2023-11-03 05:45:19 +0100

    gunner warning fixes

--- Assets/Package/PHATASS-UnityUtils/Attributes/SerializedTypeRestrictionAttribute/SampleScene/Scripts/Gunner.cs
+++ Assets/Package/PHATASS-UnityUtils/Attributes/SerializedTypeRestrictionAttribute/SampleScene/Scripts/Gunner.cs
@@ -1,3 +1,5 @@
+#nullable disable
+
 using UnityEngine;
 
 // You must import the attribute to be able to use it.
@@ -12,7 +14,6 @@ namespace PHATASS.Docs.Examples
 //
 	public class Gunner : MonoBehaviour
 	{
-	
 	// Single-Element serialization sample
 		// Editor tooltip attribute
 		[Tooltip("Single-Element serialization sample. It can be any Component or ScriptableObject that implements the interface IWeapon. IWeapon.Shoot() will be called on each frame.")]
@@ -24,10 +25,9 @@ namespace PHATASS.Docs.Examples
 		[SerializedTypeRestriction(typeof(IWeapon))]
 
 		// This is the serialized field used to store a reference to our IWeapon. We use a private backing field typed UnityEngine.Object
-		//	it must be of type UnityEngine.Object? so references to any UnityEngine.MonoBehaviour or UnityEngine.ScriptableObject objects can be stored
-		//	the ? indicates this field can contain null values
+		//	it must be of type UnityEngine.Object so references to any UnityEngine.MonoBehaviour or UnityEngine.ScriptableObject objects can be stored
 		//	note it's important to always write the full UnityEngine.Object type name to differentiate from the System.Object base class
-		private UnityEngine.Object? _mainWeapon = null;
+		private UnityEngine.Object _mainWeapon = null;
 
 		// Because the UnityEngine.Object class does not implement the IWeapon interface, casting UnityEngine.Object to IWeapon is not compiler safe code.
 		//	that's why we hide the serialized field as private and use a property that handles type checking and casting for simplicity
@@ -40,8 +40,7 @@ namespace PHATASS.Docs.Examples
 			// We use the "as" casting operator to make elements that don't implement IWeapon to be returned as null instead of causing an exception.
 			else { return this._mainWeapon as IWeapon; }
 		}}
-
-	//ENDOF Single-Element sample
+	//ENDOF Single-Element serialization sample
 
 	//MonoBehaviour lifecycle
 		private void Update ()
